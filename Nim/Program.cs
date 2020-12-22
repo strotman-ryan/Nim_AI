d226a62bbdc27385e124114219d17b2d60a17988
strotman.7 <ryan.strotman@gmail.com>
2020-12-21 19:58:58 -0500

    renamed project

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RockPileGame {
+namespace Nim {
     class Program {
         static void Main(string[] args) {
             const int numOfPiles = 5;
